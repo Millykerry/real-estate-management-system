0c9c64913a501e841dd9ed7dc725e1569911a467
brianwaithaka <waithakabrian8@gmail.com>
2025-11-29 22:45:33 +0300

    create tenant sidebar

---
+++ src/tenant/pages/TenantDashboard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router";
+
+function TenantDashboard() {
+  return (
+    <div className="grid grid-cols-5 gap-8 p-6">
+      <aside className="col-start-1 col-end-2">
+        <ul className="grid grid-cols-1 gap-8">
+          {/* <li>
+            <NavLink to="/TenantDashboard/Tenants"></NavLink>
+          </li> */}
+          <li>
+            <NavLink to="/TenantDashboard/Payment">Payment</NavLink>
+          </li>
+          <li>
+            <NavLink to="/TenantDashboard/Maintenance">Maintenance</NavLink>
+          </li>
+          <li>
+            <NavLink to="/TenantDashboard/Profile">Profile</NavLink>
+          </li>
+        </ul>
+      </aside>
+      <main className="col-start-2 col-end-6">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default TenantDashboard;
