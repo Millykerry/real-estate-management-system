f5b42414e0258df84be2bedde7d26bc33703060e
brianwaithaka <waithakabrian8@gmail.com>
2025-11-30 23:35:15 +0300

    Sending user credentials to a server for verification

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,22 +1,38 @@
-import React from "react";
-import { useNavigate } from "react-router";
+import React, { useState } from "react";
+// import { useNavigate } from "react-router";
 
 function Login() {
-  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const navigate = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
     // navigate("/AdminDashboard");
-    navigate("/AgentDashboard");
+    // navigate("/AgentDashboard");
+    // Sending user credentials to a server for verification
+    fetch(`http://localhost:7000/users?email=${email}&passsword=${password}`);
   }
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="email">Email:</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
       <button type="submit">Login</button>
